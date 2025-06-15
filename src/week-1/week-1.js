4e2cb5fb81fc971b411837f3793c441de5181483
PMS990126 <barkminsung@naver.com>
2025-06-15 22:03:22 +0900

    변수, 상수 작성

---
+++ src/week-1/week-1.js
@@ -0,0 +1,19 @@
+let userName = "박민성";
+
+const absoluteZeroCelsius = -273.15;
+
+let isLoggedIn = true;
+
+let userAge = 27;
+
+let productPrice = 39900;
+
+const backgroundColor = "#FFFFFF";
+
+let commentCount = 12;
+
+let currentPageNumber = 3;
+
+let memberGrade = "VIP";
+
+let isButtonClicked = false;
