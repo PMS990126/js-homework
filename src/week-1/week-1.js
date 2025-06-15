683b7c1093775f24a70f12d43357c99f0dce66b9
PMS990126 <barkminsung@naver.com>
2025-06-15 22:23:14 +0900

    함수 작성

--- src/week-1/week-1.js
+++ src/week-1/week-1.js
@@ -17,3 +17,65 @@ let currentPageNumber = 3;
 let memberGrade = "VIP";
 
 let isButtonClicked = false;
+
+function greetUser(username) {
+    return "안녕하세요! " + username + "님. 좋은 하루되세요!";
+}
+
+const calculateOriginalPrice = function(priceWithTax) {
+    const taxRate = 3.3; 
+    const divisor = 1 + (taxRate / 100); 
+    const originalPrice = priceWithTax / divisor; 
+  
+    return originalPrice;
+};
+
+const canSellAlcohol = (registrationCard) => {
+    const minimumAge = 19; 
+  
+    if (registrationCard && typeof registrationCard.age === 'number') {
+      return registrationCard.age >= minimumAge;
+    } else {
+      console.error("대충 예외처리");
+      return false;
+    }
+};
+  
+function getDiscountedPrice(originalPrice, discountPercent) {
+    const discountFactor = discountPercent / 100;
+  
+    const discountedPrice = originalPrice * (1 - discountFactor);
+  
+    return discountedPrice;
+}
+  
+function getGradeInfo(score) {
+    let grade = '';
+    let description = '';
+  
+    if (score >= 90 && score <= 100) {
+      grade = 'A';
+      description = '매우 우수';
+    } else if (score >= 80 && score <= 89) {
+      grade = 'B';
+      description = '우수';
+    } else if (score >= 70 && score <= 79) {
+      grade = 'C';
+      description = '보통';
+    } else if (score >= 60 && score <= 69) {
+      grade = 'D';
+      description = '미달, 통과 기준 근접';
+    } else if (score >= 0 && score <= 59) {
+      grade = 'F';
+      description = '낙제';
+    } else {
+      grade = 'Invalid';
+      description = '대충 예외처리';
+    }
+  
+    return {
+      score: score,
+      grade: grade,
+      description: description
+    };
+} 
